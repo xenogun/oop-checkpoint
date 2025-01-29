2e64bb9c251cd5e5205654d1cc4d5368657a16e2
xenogun <idirkeddache@gmail.com>
2025-01-29 13:22:35 +0100

    updated

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ class ShoppingCart {
     this.updateTotalPrice();
   }
 
+  // selecting the prices 
   updateTotalPrice() {
     const totalPriceElement = document.querySelector(".total");
     const unitPrices = document.querySelectorAll(".unit-price");
     const quantities = document.querySelectorAll(".quantity");
     let total = 0;
-
+// logic of the calculation
     unitPrices.forEach((priceElement, index) => {
       const unitPrice = parseFloat(priceElement.textContent.replace("$", ""));
       const quantity = parseInt(quantities[index].textContent);
@@ -19,13 +20,18 @@ class ShoppingCart {
     totalPriceElement.textContent = `${total} $`;
   }
 
+  // add quatity and remove quandtity
   updateQuantity(event) {
-    const quantityElement = event.target.parentElement.querySelector(".quantity");
+    const quantityElement =
+      event.target.parentElement.querySelector(".quantity");
     let quantity = parseInt(quantityElement.textContent);
 
     if (event.target.classList.contains("fa-plus-circle")) {
       quantity += 1;
-    } else if (event.target.classList.contains("fa-minus-circle") && quantity > 0) {
+    } else if (
+      event.target.classList.contains("fa-minus-circle") &&
+      quantity > 0
+    ) {
       quantity -= 1;
     }
 
@@ -33,27 +39,33 @@ class ShoppingCart {
     this.updateTotalPrice();
   }
 
+  // remove items card
   removeItem(event) {
-    const card = event.target.closest(".card-body");
+    const card = event.target.closest(".card");
     card.remove();
     this.updateTotalPrice();
   }
-
+// like items
   toggleLike(event) {
     event.target.classList.toggle("liked");
-    event.target.style.color = event.target.classList.contains("liked") ? "red" : "black";
+    event.target.style.color = event.target.classList.contains("liked")
+      ? "red"
+      : "black";
   }
 
+  // add event listeners for buttons and icons
   initEventListeners() {
-    document.querySelectorAll(".fa-plus-circle, .fa-minus-circle").forEach(button => {
-      button.addEventListener("click", this.updateQuantity.bind(this));
-    });
+    document
+      .querySelectorAll(".fa-plus-circle, .fa-minus-circle")
+      .forEach((button) => {
+        button.addEventListener("click", this.updateQuantity.bind(this));
+      });
 
-    document.querySelectorAll(".fa-trash-alt").forEach(trashIcon => {
+    document.querySelectorAll(".fa-trash-alt").forEach((trashIcon) => {
       trashIcon.addEventListener("click", this.removeItem.bind(this));
     });
 
-    document.querySelectorAll(".fa-heart").forEach(heartIcon => {
+    document.querySelectorAll(".fa-heart").forEach((heartIcon) => {
       heartIcon.addEventListener("click", this.toggleLike.bind(this));
     });
   }
